811c0cdfca6482041eb96cd8a787446ed69de9bf
Saifuzzaman <pmsaifuzzaman@gmail.com>
2023-11-25 16:55:05 +0600

    Dotenv added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
 const cors = require('cors');
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -8,11 +9,10 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-// blissfulMatch
-// eDWB1n20OKuNZmmj
 
 
-const uri = "mongodb+srv://blissfulMatch:eDWB1n20OKuNZmmj@cluster0.ublbqgg.mongodb.net/?retryWrites=true&w=majority";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ublbqgg.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
