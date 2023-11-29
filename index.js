77d1ff749257879179053665578ba6bf926a3caf
Saifuzzaman <pmsaifuzzaman@gmail.com>
2023-11-29 16:36:50 +0600

    Some problem fixed

--- index.js
+++ index.js
@@ -39,7 +39,36 @@ async function run() {
     })
 
     // middlewares 
+    // const verifyToken = (req, res, next) => {
+    //   console.log('inside verify token', req.headers.authorization);
+    //   if (!req.headers.authorization) {
+    //     return res.status(401).send({ message: 'unauthorized access' });
+    //   }
+    //   const token = req.headers.authorization.split(' ')[1];
+    //   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    //     if (err) {
+    //       return res.status(401).send({ message: 'unauthorized access' })
+    //     }
+    //     req.decoded = decoded;
+    //     next();
+    //   })
+    // }
+
+    // use verify admin after verifyToken
+    // const verifyAdmin = async (req, res, next) => {
+    //   const email = req.decoded.email;
+    //   const query = { email: email };
+    //   const user = await manageUserCollection.findOne(query);
+    //   const isAdmin = user?.role === 'admin';
+    //   if (!isAdmin) {
+    //     return res.status(403).send({ message: 'forbidden access' });
+    //   }
+    //   next();
+    // }
+
+    // Middlewares
     const verifyToken = (req, res, next) => {
+      console.log('Decoded Token:', req.decoded);
       console.log('inside verify token', req.headers.authorization);
       if (!req.headers.authorization) {
         return res.status(401).send({ message: 'unauthorized access' });
@@ -47,12 +76,31 @@ async function run() {
       const token = req.headers.authorization.split(' ')[1];
       jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if (err) {
-          return res.status(401).send({ message: 'unauthorized access' })
+          return res.status(401).send({ message: 'unauthorized access' });
         }
         req.decoded = decoded;
         next();
-      })
-    }
+      });
+    };
+
+    // use verifyToken before verifyAdmin
+    const verifyAdmin = async (req, res, next) => {
+      // Ensure that req.decoded is set by verifyToken middleware
+      if (!req.decoded || !req.decoded.ContactEmail) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+
+      const email = req.decoded.ContactEmail;  // Use ContactEmail consistently
+      const query = { email: email };
+      const user = await manageUserCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    };
+
+
 
 
     // Biodata related api
@@ -61,10 +109,10 @@ async function run() {
       // console.log(req.headers)
       const result = await biodataCollection.find().toArray();
       res.send(result);
-    });  
+    });
 
     // User related api
-    app.get("/users", async(req, res) => {
+    app.get("/users", async (req, res) => {
       const result = await manageUserCollection.find().toArray();
       res.send(result)
     });
@@ -80,6 +128,24 @@ async function run() {
       const result = await manageUserCollection.insertOne(user);
       res.send(result);
     });
+
+    //Admin email get 
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;  // Corrected from req.params.ContactEmail
+      console.log(req.decoded.ContactEmail)
+      if (email !== req.decoded.ContactEmail) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+
+      const query = { email: email };
+      const user = await manageUserCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
+    })
+
 
     // Make admin a User
     app.patch('/users/admin/:id', async (req, res) => {
@@ -147,7 +213,7 @@ async function run() {
       const id = req.params.id;
       const result = await favouriteCollection.deleteOne({ _id: new ObjectId(id) });
       res.send(result);
-      });
+    });
 
 
 
